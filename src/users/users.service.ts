3b7bd507d4b916ab853bb6b25e10a2d727003bd4
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-01-22 14:58:35 +0545

    feat: integrate Mongoose for user management; add User schema and service methods for user creation with validation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,13 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { userDto as UserDto } from 'src/dto/users.dto';
 import * as bcrypt from 'bcrypt';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from 'src/schemas/user.schema';
+import { Model } from 'mongoose';
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
   getHello(): string {
     return 'Hello World! from UserService';
   }
@@ -20,7 +25,27 @@ export class UsersService {
     user.password = hashedPassword;
     user.confirmPassword = hashedConfirmPassword;
 
-    return 'User created successfully';
+    //checking if user already exists
+
+    const isUserExist = await this.userModel.findOne({ email: user.email });
+
+    if (isUserExist) {
+      throw new HttpException(
+        { message: 'User already exists' },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const newUser = new this.userModel(user);
+    try {
+      await newUser.save();
+      return { message: 'User created successfully' };
+    } catch {
+      throw new HttpException(
+        { message: 'Error creating user' },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   private async hashPassword(password: string): Promise<string> {
