82f278e3b8031cee0b0aa871526dd3a29de1dd15
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-01-22 13:28:16 +0545

    feat: implement user module with controller, service, and DTO; add validation and bcrypt for password handling

---
+++ src/dto/users.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+export class userDto {
+  @IsString()
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters' })
+  password: string;
+  @IsString()
+  @MinLength(8, { message: 'confirmPassword must be at least 8 characters' })
+  confirmPassword: string;
+}
